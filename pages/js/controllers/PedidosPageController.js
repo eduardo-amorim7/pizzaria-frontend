4a89cd4119f963479f88e8b11d88e63f24b0d70f
Eduardo Amorim <enapoli7@gmail.com>
2025-07-26 13:14:57 -0300

    ajustes

--- pages/js/controllers/PedidosPageController.js
+++ pages/js/controllers/PedidosPageController.js
@@ -1,92 +1,8 @@
 /*global angular,$,addInteraction,L,document*/
 angular.module('vdo.spa').controller('PedidosPageController', function($scope, $rootScope, $timeout, $http, $log, $location, $route, $routeParams, $window, $filter) {
 
-  var $branch = this;
+  var $orders = this;
   var lastRoute = $route.current;
-  $scope.loadingPositions = true;
-  $scope.textFilter = "";
-
-  $scope.pages = [];
-  $scope.paginatedItems = [];
-
-  $scope.pageSize = parseInt($window.localStorage.getItem('bc.page.itens.size')) || 10;
-
-  $scope.currentPage = 0;
-  $scope.maxPagesToShow = 5;
-
-  $scope.changeFilter = function(text){
-    $scope.textFilter = text;
-    $scope.currentPage = 0;
-    $scope.paginateItems();
-  };
-
-  // Função para calcular os itens na página atual
-  $scope.paginateItems = function() {
-    var filteredItems = $scope.textFilter ? $filter('filter')($scope.branchs, $scope.textFilter) : $scope.branchs;
-
-    var startIndex = $scope.currentPage * $scope.pageSize;
-    $scope.paginatedItems = filteredItems.slice(startIndex, startIndex + $scope.pageSize);
-    $scope.totalPages = Math.ceil(filteredItems.length / $scope.pageSize);
-    $scope.updatePages();
-  };
-
-
-  $scope.updatePages = function() {
-    $scope.pages = [];
-    var startPage, endPage;
-
-    if ($scope.totalPages <= $scope.maxPagesToShow) {
-      startPage = 0;
-      endPage = $scope.totalPages;
-    } else {
-      var halfPagesToShow = Math.floor($scope.maxPagesToShow / 2);
-      startPage = Math.max(0, $scope.currentPage - halfPagesToShow);
-      endPage = Math.min(startPage + $scope.maxPagesToShow, $scope.totalPages);
-
-      if (endPage - startPage < $scope.maxPagesToShow) {
-        startPage = endPage - $scope.maxPagesToShow;
-      }
-    }
-
-    for (var i = startPage; i < endPage; i++) {
-      $scope.pages.push(i);
-    }
-  };
-
-  $scope.goToPage = function(page) {
-    $scope.currentPage = page;
-    $scope.paginateItems();
-    $scope.updatePages();
-  };
-
-
-  $scope.nextPage = function() {
-    if ($scope.currentPage < Math.ceil($scope.branchs.length / $scope.pageSize) - 1) {
-      $scope.currentPage++;
-      $scope.paginateItems();
-    }
-  };
-
-
-  $scope.prevPage = function() {
-    if ($scope.currentPage > 0) {
-      $scope.currentPage--;
-      $scope.paginateItems();
-    }
-  };
-
-  $scope.changepageSize = function(size){
-    if ($window && $window.localStorage){
-      $window.localStorage.setItem('bc.page.itens.size', size);  
-    } 
-
-    $scope.pageSize = size;
-    $scope.currentPage = 0;
-    $scope.updatePages();
-    $scope.paginateItems();
-  };
-
-
 
   $scope.vm = {
     'active': { 'id': $routeParams.id, 'slug': $routeParams.slug }
@@ -94,31 +10,64 @@ angular.module('vdo.spa').controller('PedidosPageController', function($scope, $
 
   $scope.edit = function(id, slug) {
     /*global history*/
-    $location.path('/branches/edit/'+slug);
+    $location.path('/pedidos/edit/'+slug);
   };
-  $scope.backToLast = function() {
-    /*global history*/
-    $window.history.back();
-  };
-
-
 
   $scope.back = function(id, slug) {
     /*global history*/
-    $location.path('/branches');
+    $location.path('/pedidos');
   };
 
-  function findBranchs() {
-    
-$scope.loadingPositions = false;
-$scope.branchs = [];
+  function findPedidos() {
+    $scope.loadingPositions = false;
+
+    $scope.pedidos = [
+      {
+        id: '018',
+        status: 'Aprovado',
+        type: 'Entrega',
+        source: 'LOCAL',
+        time: '10:16',
+        client: 'Marcio',
+        phone: '(61) 99673-6411',
+        address: 'Taguatinga',
+        deliveryTime: '5min',
+        preparationTime: '1799 min'
+      },
+      {
+        id: '019',
+        status: 'Aprovado',
+        type: 'Entrega',
+        source: 'LOCAL',
+        time: '10:16',
+        client: 'Marcio',
+        phone: '(61) 99673-6411',
+        address: 'Taguatinga',
+        deliveryTime: '5min',
+        preparationTime: '1799 min'
+      },
+      {
+        id: '020',
+        status: 'Aprovado',
+        type: 'Entrega',
+        source: 'SITE',
+        time: '10:16',
+        client: 'Marcio',
+        phone: '(61) 99673-6411',
+        address: 'Taguatinga',
+        deliveryTime: '5min',
+        preparationTime: '1799 min'
+      }
+    ];
+
   }
 
 
   if(!$scope.vm.active.id){
-    findBranchs();
+    findPedidos();
   }
 
-  return $branch;
+  return $orders;
 });
+
 
